d604f54335ad933e927a2b230ca7874a95315878
Jhongert Fuertes <jfuertes@Jhongerts-MacBook-Pro.local>
2017-09-19 12:16:03 -0400

    Fix banner

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = Promise;
 
-mongoose.connect('mongodb://localhost/techscraperdb');
+//mongoose.connect('mongodb://localhost/techscraperdb');
+mongoose.connect('mongodb://heroku_h791bs5z:455u1p927e5m2kabbrtap9vdor@ds121014.mlab.com:21014/heroku_h791bs5z');
 
 var db = mongoose.connection;
 
